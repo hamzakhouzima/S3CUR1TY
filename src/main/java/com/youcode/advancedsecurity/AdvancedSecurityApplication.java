82db3763fd4f57886dbec558001f3f506ebd22cc
hamzakhouzima <khouzimahamza20@gmail.com>
2024-01-30 10:37:17 +0100

    setup

---
+++ src/main/java/com/youcode/advancedsecurity/AdvancedSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.youcode.advancedsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdvancedSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/youcode/advancedsecurity/AdvancedSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youcode.advancedsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvancedSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
